18eeb9cd9801a45df1f9111e9afd4c15ac1e3a90
Pashua85 <pavellashuk85@gmail.com>
2020-06-10 00:35:18 +0500

    add properties amount to props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Main from '../main/main';
 
-const App = () => {
-  return <Main />;
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {propertiesAmount} = props;
+  return <Main propertiesAmount={propertiesAmount} />;
 };
 
 export default App;
