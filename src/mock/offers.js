b613d96c6c022d85fbdfd1eacce50dcf0ddbd5a1
Pashua85 <pavellashuk85@gmail.com>
2020-06-17 10:44:57 +0500

    add mock data

---
+++ src/mock/offers.js
@@ -0,0 +1,37 @@
+import {v4 as uuidv4} from 'uuid';
+
+export default [
+  {
+    propertyName: `Beautiful luxurious apartment at great location`,
+    propertyType: `apartment`,
+    price: 120,
+    rating: 4,
+    isPremium: true,
+    id: uuidv4()
+  },
+  {
+    propertyName: `Wood and stone place`,
+    propertyType: `private room`,
+    price: 80,
+    rating: 4,
+    isPremium: false,
+    id: uuidv4()
+  },
+  {
+    propertyName: `Canal View Prinsengracht`,
+    propertyType: `apartment`,
+    price: 132,
+    rating: 4,
+    isPremium: false,
+    id: uuidv4()
+  },
+  {
+    propertyName: `Nice, cozy, warm big bed apartment`,
+    propertyType: `apartment`,
+    price: 180,
+    rating: 5,
+    isPremium: true,
+    id: uuidv4()
+  }
+];
+
