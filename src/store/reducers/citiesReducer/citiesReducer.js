c166a82fe5ef2daae3a2e0f648d50d7a6181e3f3
Pashua85 <pavellashuk85@gmail.com>
2020-07-13 18:41:33 +0500

    add set cities from loading offers

--- src/store/reducers/citiesReducer/citiesReducer.js
+++ src/store/reducers/citiesReducer/citiesReducer.js
@@ -1,4 +1,21 @@
-function citiesReducer(state = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`]) {
+import {LOAD_OFFERS} from '../../action-types/action-types';
+
+let tmpArray = [];
+function checkOffer(offer) {
+  if (tmpArray.find((of) => of.city.name === offer.city.name) === undefined) {
+    tmpArray.push(offer);
+    return true;
+  }
+  return false;
+}
+
+function citiesReducer(state = [], action) {
+  switch (action.type) {
+    case LOAD_OFFERS: {
+      const offersWithUniqueCity = action.payload.filter((offer) => checkOffer(offer));
+      return offersWithUniqueCity.map((of) => of.city);
+    }
+  }
   return state;
 }
 
