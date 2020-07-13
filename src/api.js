f67ffeb3b90808f93f7f9f13303ca7fe853bbe7a
Pashua85 <pavellashuk85@gmail.com>
2020-07-13 14:18:56 +0500

    add createAPI

---
+++ src/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const Error = {
+  UNAUTHORIZED: 401
+};
+
+
+export const createAPI = (dispatch) => {
+  const api = axios.create({
+    baseURL: `https://4.react.pages.academy/six-cities`,
+    timeout: 5000,
+    withCredentials: true
+  });
+
+  const onSucess = (response) => {
+    return response;
+  };
+
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      dispatch();
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSucess, onFail);
+
+  return api;
+};
--- src/store/reducers/offersReducer/offersReducer.js
+++ src/store/reducers/offersReducer/offersReducer.js
@@ -1,6 +1,12 @@
 import offers from '../../../mock/offers';
+import LOAD_OFFERS from '../../../store/action-types/action-types';
 
-function offersReducer(state = offers) {
+function offersReducer(state = offers, action) {
+  switch (action.type) {
+    case LOAD_OFFERS: {
+      return action.payload;
+    }
+  }
   return state;
 }
 
