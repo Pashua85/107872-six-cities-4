3de093bff2237c33c30da10bb10b842c7c743d69
Pashua85 <pavellashuk85@gmail.com>
2020-07-15 22:17:06 +0500

    add test for authStatusReducer

--- src/store/reducers/activeOfferReducer/activeOfferReducer.test.js
+++ src/store/reducers/activeOfferReducer/activeOfferReducer.test.js
@@ -2,7 +2,7 @@ import activeOfferReducer from './activeOfferReducer';
 import offers from '../../../mock/test-offers';
 
 describe(`activeOfferReducer`, () => {
-  test(`When it is called with offer-1-object as state and object { type: "SET_ACTIVE_OFFER, offer: offer-2-object }, it should return offer-2-object`, () => {
+  test(`When it is called with offer-1-object as state and object { type: "SET_ACTIVE_OFFER", offer: offer-2-object }, it should return offer-2-object`, () => {
     const offer1 = offers[0];
     const offer2 = offers[1];
     const result = activeOfferReducer(offer1, {
@@ -12,7 +12,7 @@ describe(`activeOfferReducer`, () => {
     expect(result).toEqual(offer2);
   });
 
-  test(`When it is called with offer-1-object as state and object { type: "SET_SOME_OFFER, offer: offer-2-object }, it should return offer-1-object`, () => {
+  test(`When it is called with offer-1-object as state and object { type: "SET_SOME_OFFER", offer: offer-2-object }, it should return offer-1-object`, () => {
     const offer1 = offers[0];
     const offer2 = offers[1];
     const result = activeOfferReducer(offer1, {
--- src/store/reducers/currentCityReducer/currentCityReducer.test.js
+++ src/store/reducers/currentCityReducer/currentCityReducer.test.js
@@ -1,19 +1,23 @@
 import currentCityReducer from './currentCityReducer';
+import cities from '../../../mock/cities';
 
 describe(`currentCityReducer`, () => {
-  test(`When it is called with "London" as state and object { type: "SET_CURRENT_CITY", city: "Berlin" } as action, it should return "Berlin"`, () => {
-    const result = currentCityReducer(`London`, {
+  const city0 = cities[0];
+  const city1 = cities[1];
+
+  test(`When it is called with city-0-object as state and object { type: "SET_CURRENT_CITY", city: city-1-object } as action, it should return city-1-object`, () => {
+    const result = currentCityReducer(city0, {
       type: `SET_CURRENT_CITY`,
-      city: `Berlin`
+      city: city1
     });
-    expect(result).toBe(`Berlin`);
+    expect(result).toEqual(city1);
   });
 
-  test(`When it is called with "London" as state and object { type: "SET_CURRENT_TOWN", city: "Berlin" } as action, it should return "London"`, () => {
-    const result = currentCityReducer(`London`, {
+  test(`When it is called with "London" as state and object { type: "SET_CURRENT_TOWN", city: city-1-object } as action, it should return city-0-object`, () => {
+    const result = currentCityReducer(city0, {
       type: `SET_CURRENT_TOWN`,
-      city: `Berlin`
+      city: city1
     });
-    expect(result).toBe(`London`);
+    expect(result).toEqual(city0);
   });
 });
