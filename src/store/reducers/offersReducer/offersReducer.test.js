20cb9e65e4abd23a24353d3b8d36a0872342ce22
Pashua85 <pavellashuk85@gmail.com>
2020-07-15 22:33:56 +0500

    add tests for offersReducer

---
+++ src/store/reducers/offersReducer/offersReducer.test.js
@@ -0,0 +1,21 @@
+import {LOAD_OFFERS, SET_CURRENT_CITY} from '../../action-types/action-types';
+import offersReducer from './offersReducer';
+import offers from '../../../mock/test-offers';
+
+describe(`offersReducer`, () => {
+  test(`When it is called with [] as state and object {type: ${LOAD_OFFERS}, payload: offers}, it should return offers`, () => {
+    const result = offersReducer([], {
+      type: LOAD_OFFERS,
+      payload: offers
+    });
+    expect(result).toEqual(offers);
+  });
+
+  test(`When it is called with [] as state and object {type: ${SET_CURRENT_CITY}, payload: offers}, it should return []`, () => {
+    const result = offersReducer([], {
+      type: SET_CURRENT_CITY,
+      payload: offers
+    });
+    expect(result).toEqual([]);
+  });
+});
