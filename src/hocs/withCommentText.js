6d8dfa368ba402c600ab07ab9e1403a4153d2be5
Pashua85 <pavellashuk85@gmail.com>
2020-07-17 00:29:41 +0500

    add hoc withCommentText

---
+++ src/hocs/withCommentText.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const withCommentText = (Component) => {
+  class WithCommentText extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.checkIsDisabled = this.checkIsDisabled.bind(this);
+      this.handleCommentTextChange = this.handleCommentTextChange.bind(this);
+      this.handleRatingChange = this.handleRatingChange.bind(this);
+
+      this.state = {
+        commentText: ``,
+        rating: 0,
+        disabled: true
+      };
+    }
+
+    checkIsDisabled() {
+      if (this.state.commentText.length >= 50 && this.state.commentText.length <= 300) {
+        this.setState({
+          disabled: false
+        });
+      } else {
+        this.setState({
+          disabled: true
+        });
+      }
+    }
+
+    handleCommentTextChange(e) {
+      this.setState({
+        commentText: e.target.value
+      });
+      this.checkIsDisabled();
+    }
+
+    handleRatingChange(e) {
+      if (e.target.checked) {
+        this.setState({
+          rating: parseInt(e.target.value, 10)
+        });
+      }
+    }
+
+    render() {
+      const {commentText, disabled} = this.state;
+      return (
+        <Component
+          {...this.props}
+          commentText={commentText}
+          disabled={disabled}
+          onCommentTextChange={this.handleCommentTextChange}
+          onRatingChange={this.handleRatingChange}
+        />
+      );
+    }
+  }
+  return WithCommentText;
+};
+
+export default withCommentText;
+
