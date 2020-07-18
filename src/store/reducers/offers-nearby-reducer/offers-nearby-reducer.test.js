1679706360b31e1b21a7af7eb50b09ba7372e05b
Pashua85 <pavellashuk85@gmail.com>
2020-07-18 14:42:19 +0500

    add tests for offersNearbyReducer

---
+++ src/store/reducers/offers-nearby-reducer/offers-nearby-reducer.test.js
@@ -0,0 +1,20 @@
+import offersNearbyReducer from './offers-nearby-reducer';
+import offers from '../../../mock/test-offers';
+
+describe(`offersNearbyReducer`, () => {
+  test(`When it is called with [] as state and object { type: "LOAD_OFFERS_NEARBY, offers: offers} as action, it should return [offers]`, () => {
+    const result = offersNearbyReducer([], {
+      type: `LOAD_OFFERS_NEARBY`,
+      offers
+    });
+    expect(result).toEqual(offers);
+  });
+
+  test(`When it is called with [] as state and object { type: "LOAD_OFFERS_FARAWAY, offers: offers} as action, it should return []`, () => {
+    const result = offersNearbyReducer([], {
+      type: `LOAD_OFFERS_FARAWAY`,
+      offers
+    });
+    expect(result).toEqual([]);
+  });
+});
