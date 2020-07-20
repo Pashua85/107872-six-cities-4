dab45919e78dd92ba5567d6d06b0a86cc28c0a9f
Pashua85 <pavellashuk85@gmail.com>
2020-07-20 11:53:26 +0500

    add sorting places

--- src/store/reducers/offersReducer/selectors.js
+++ src/store/reducers/offersReducer/selectors.js
@@ -1,6 +1,7 @@
 import {createSelector} from 'reselect';
 import NameSpace from '../name-space';
 import {getCurrentCity} from '../currentCityReducer/selectors';
+import {getSortingOption} from '../sortingOptionReducer/selectors';
 import {createOffer} from '../../../adapters/offer';
 
 const getOffers = (state) => {
@@ -26,4 +27,33 @@ export const getOfferByParamsId = (state, id) => {
   return createOffer(offer);
 };
 
+export const getSortedCityOffers = createSelector(
+    getSortingOption,
+    getCityOffers,
+    (option, offers) => {
+      switch (option) {
+        case `Popular`: {
+          return offers;
+        }
+        case `Price: low to high`: {
+          return offers.sort((of1, of2) => {
+            return of1.price - of2.price;
+          });
+        }
+        case `Price: high to low`: {
+          return offers.sort((of1, of2) => {
+            return of2.price - of1.price;
+          });
+        }
+        case `Top rated first`: {
+          return offers.sort((of1, of2) => {
+            return of2.rating - of1.rating;
+          });
+        }
+        default: {
+          return offers;
+        }
+      }
+    }
+);
 
